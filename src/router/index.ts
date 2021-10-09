865f419d010bb428d398bf0bc23bfac311ba1442
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-10-09 15:24:25 -0300

    Agregadas rutas para cada paso del contrato de trabajo

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,39 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
+import FormDatosRemuneracion from '@/views/FormDatosRemuneracion.vue'
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
     component: Home
+  },
+  {
+    path: '/contrato-trabajo',
+    name: 'Home',
+    component: () => import('@/views/GenerarContrato.vue'),
+    children: [
+      {
+        path: '1',
+        component: () => import('@/views/FormDatosEmpleador.vue')
+      },
+      {
+        path: '2',
+        component: () => import('@/views/FormDatosTrabajador.vue')
+      },
+      {
+        path: '3',
+        component: () => import('@/views/FormDatosServicios.vue')
+      },
+      {
+        path: '4',
+        component: () => import('@/views/FormDatosJornada.vue')
+      },
+      {
+        path: '5',
+        component: FormDatosRemuneracion
+      }
+    ]
   },
   {
     path: '/test',
