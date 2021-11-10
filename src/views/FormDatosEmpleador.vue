22172cdd4ec52f50f329f521f436a455c39c15a3
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-10 11:16:27 -0300

    Agregadas computed properties

--- src/views/FormDatosEmpleador.vue
+++ src/views/FormDatosEmpleador.vue
@@ -2,51 +2,90 @@
   <form class="form">
     <div class="flex w-full">
       <div>
-        <input type="radio" id="juridica" value="true" v-model="empleador.esPersonaJuridica"/>
+        <input
+          id="juridica"
+          v-model="esPersonaJuridica"
+          type="radio"
+          :value="true"
+        />
         <label for="juridica">Persona Juridica</label>
       </div>
       <div>
-        <input type="radio" id="natural" value="false" v-model="empleador.esPersonaJuridica">
+        <input
+          id="natural"
+          v-model="esPersonaJuridica"
+          type="radio"
+          :value="false"
+        >
         <label for="natural">Persona Natural</label>
       </div>
     </div>
-    <div class="w-1/3 input-field">
-      <label for="rut" class="text-left">Rut</label>
-      <input class="border" type="text" id="rut"/>
+    <div class="w-1/3 input-field test">
+      <label
+        for="rut"
+        class="text-left"
+      >Rut</label>
+      <input
+        v-model="rut"
+        class="border"
+        type="text"
+        placeholder="Rut"
+      />
     </div>
     <div class="w-2/3 input-field">
       <label for="razonSocial">Razón Social o Nombre</label>
-      <input type="text" id="razonSocial">
+      <input
+        v-model="razonSocial"
+        type="text"
+      >
     </div>
     <div class="w-1/3 input-field">
       <label for="direccion">Dirección</label>
-      <input type="text" >
+      <input
+        v-model="direccion"
+        type="text"
+      >
     </div>
     <div class="w-1/3 input-field">
       <label for="region">Región</label>
-      <RegionDropdown v-model:region="regionSeleccionada"/>
+      <RegionDropdown v-model:region="regionSeleccionada" />
     </div>
     <div class="w-1/3 input-field">
       <label for="comuna">Comuna</label>
-      <ComunaDropdown :region="regionSeleccionada"/>
+      <ComunaDropdown
+        v-model:comuna="comunaSeleccionada"
+        :region="regionSeleccionada.codigo"
+      />
     </div>
     <div class="w-full input-field">
-      <label for="repLegal">Nombre Rep. Legal</label>
-      <input type="text">
+      <label for="nombRepLegal">Nombre Rep. Legal</label>
+      <input
+        v-model="nombRepLegal"
+        type="text"
+        :disabled="!esPersonaJuridica"
+      >
     </div>
     <div class="w-1/3 input-field">
-      <label for="rutRepLegal">RUN Rep. Legal</label>
-      <input type="text">
+      <label for="runRepLegal">RUN Rep. Legal</label>
+      <input
+        v-model="runRepLegal"
+        type="text"
+        :disabled="!esPersonaJuridica"
+      >
     </div>
     <div class="w-2/3 input-field">
       <label for="cargoRepLegal">Cargo Rep. Legal (Opcional)</label>
-      <input type="text">
+      <input
+        v-model="cargoRepLegal"
+        type="text"
+        :disabled="!esPersonaJuridica"
+      >
     </div>
   </form>
 </template>
 
-<script>
-import { defineComponent, ref } from 'vue'
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
 
 import store from '@/store/contrato'
 import RegionDropdown from '@/components/RegionDropdown.vue'
@@ -57,13 +96,106 @@ export default defineComponent({
     RegionDropdown,
     ComunaDropdown
   },
-  setup () {
+  data() {
     const empleador = store.state.empleador
-    const regionSeleccionada = ref('')
+    const regionSeleccionada = computed({
+      get: () => empleador.region,
+      set: (nuevaReg) => {
+        empleador.region = nuevaReg
+      }
+    })
+
+    const comunaSeleccionada = computed({
+      get: () => empleador.comuna,
+      set: (nuevaCom) => {
+        empleador.comuna = nuevaCom
+      }
+    })
+
+    const esPersonaJuridica = computed({
+      get: (): boolean => {
+        return empleador.esPersonaJuridica
+      },
+      set: (nuevoValor: boolean) => {
+        empleador.esPersonaJuridica = nuevoValor
+      }
+    })
+
+    const rut = computed({
+      get: () => {
+        if (empleador.rut) {
+          return empleador.rut
+        }
+        return ''
+      },
+      set: (nuevoRut: string) => {
+        empleador.rut = nuevoRut
+      }
+    })
+
+    const razonSocial = computed({
+      get: () => {
+        if (empleador.razonSocial) {
+          return empleador.razonSocial
+        }
+        return ''
+      },
+      set: (nuevaRazon) => {
+        empleador.razonSocial = nuevaRazon
+      }
+    })
+
+    const nombRepLegal = computed({
+      get: () => empleador.nombreRepLegal,
+      set: (nuevoNomb) => {
+        empleador.nombreRepLegal = nuevoNomb
+      }
+    })
+
+    const direccion = computed({
+      get: () => empleador.domicilio,
+      set: (nuevoDom) => {
+        empleador.domicilio = nuevoDom
+      }
+    })
+
+    const runRepLegal = computed({
+      get: () => empleador.rutRepLegal,
+      set: (nuevoRut) => {
+        empleador.rutRepLegal = nuevoRut
+      }
+    })
+
+    const cargoRepLegal = computed({
+      get: () => empleador.cargoRepLegal,
+      set: (nuevoCargo) => {
+        empleador.cargoRepLegal = nuevoCargo
+      }
+    })
     return {
       empleador,
-      regionSeleccionada
+      regionSeleccionada,
+      comunaSeleccionada,
+      esPersonaJuridica,
+      rut,
+      razonSocial,
+      direccion,
+      nombRepLegal,
+      runRepLegal,
+      cargoRepLegal
     }
   }
 })
 </script>
+<style scoped>
+.test {
+  position: relative;
+}
+.test > input::placeholder {
+  opacity: 0;
+}
+.test > label + input:not(:placeholder-shown),
+.test > label + input:focus:not(:placeholder-shown) {
+  background: red;
+}
+</style>
--- src/views/FormDatosTrabajador.vue
+++ src/views/FormDatosTrabajador.vue
@@ -2,68 +2,164 @@
   <form class="form">
     <div class="input-field w-full">
       <label for="run">RUN</label>
-      <input type="text" class="w-1/3">
+      <input
+        v-model="run"
+        type="text"
+        class="w-1/3"
+      >
     </div>
     <div class="input-field w-1/3">
       <label for="nombres">Nombres</label>
-      <input type="text">
+      <input
+        v-model="nombres"
+        type="text"
+      >
     </div>
     <div class="input-field w-1/3">
       <label for="apellidoP">Apellido Paterno</label>
-      <input type="text">
+      <input
+        v-model="apellidoP"
+        type="text"
+      >
     </div>
     <div class="input-field w-1/3">
       <label for="apellidoM">Apellido Materno</label>
-      <input type="text">
+      <input
+        v-model="apellidoM"
+        type="text"
+      >
     </div>
     <div class="input-field">
       <label for="nacionalidad">Nacionalidad</label>
-      <input type="text">
+      <input
+        v-model="nacionalidad"
+        type="text"
+      >
     </div>
     <div class="w-2/3 flex">
       <div class="input-field">
         <label for="estadoCivil">Estado Civil</label>
-        <select name="estadoCivil" id="estadoCivil">
-          <option value="casado" selected>Casado/a</option>
-          <option value="soltero">Soltero/a</option>
-          <option value="viudo">Viudo/a</option>
+        <select
+          v-model="estadoCivil"
+          name="estadoCivil"
+        >
+          <option
+            v-for="estado in EstadoCivil"
+            :key="estado"
+            :value="estado"
+          >
+            {{ estado }}
+          </option>
         </select>
       </div>
       <div class="input-field">
         <label for="fechaNac">Fecha Nacimiento</label>
-        <input type="date" name="fechaNac" id="fechaNac">
+        <input
+          v-model="fechaNac"
+          type="date"
+        >
       </div>
     </div>
     <div class="input-field">
       <label for="direccion">Dirección</label>
-      <input type="text">
+      <input
+        v-model="direccion"
+        type="text"
+      >
     </div>
     <div class="input-field w-1/3">
       <label for="region">Región</label>
-      <RegionDropdown v-model:region="regionSeleccionada"/>
+      <RegionDropdown v-model:region="regionSeleccionada" />
     </div>
     <div class="input-field w-1/3">
       <label for="comuna">Comuna</label>
-      <ComunaDropdown :region="regionSeleccionada"/>
+      <ComunaDropdown
+        v-model:comuna="comunaSeleccionada"
+        :region="regionSeleccionada.codigo"
+      />
     </div>
   </form>
 </template>
 
 <script lang="ts">
+import { computed, defineComponent } from 'vue'
 import RegionDropdown from '@/components/RegionDropdown.vue'
 import ComunaDropdown from '@/components/ComunaDropdown.vue'
 
-import { defineComponent, ref } from 'vue'
-
+import store from '@/store/contrato'
+import { EstadoCivil } from '@/enums'
 export default defineComponent({
   components: {
     RegionDropdown,
     ComunaDropdown
   },
-  setup () {
-    const regionSeleccionada = ref('0')
+  setup() {
+    const trabajador = store.state.trabajador
+    const regionSeleccionada = computed({
+      get: () => store.get('trabajador', 'region'),
+      set: (nuevaRegion) => {
+        trabajador.region = nuevaRegion
+      }
+    })
+    const comunaSeleccionada = computed({
+      get: () => store.get('trabajador', 'comuna'),
+      set: (nuevaComuna) => {
+        trabajador.comuna = nuevaComuna
+      }
+    })
+
+    const run = computed({
+      get: () => store.get('trabajador', 'run'),
+      set: (nuevoRun) => { trabajador.run = nuevoRun }
+    })
+
+    const nombres = computed({
+      get: () => store.get('trabajador', 'nombre'),
+      set: (nuevoNomb) => store.set('trabajador', 'nombre', nuevoNomb)
+    })
+
+    const apellidoP = computed({
+      get: () => store.get('trabajador', 'apellidoPaterno'),
+      set: (nuevoApellido) => { trabajador.apellidoPaterno = nuevoApellido }
+    })
+
+    const apellidoM = computed({
+      get: () => store.get('trabajador', 'apellidoMaterno'),
+      set: (nuevoApellido) => { trabajador.apellidoMaterno = nuevoApellido }
+    })
+
+    const estadoCivil = computed({
+      get: () => store.get('trabajador', 'estadoCivil'),
+      set: (nuevoEstado) => { trabajador.estadoCivil = nuevoEstado }
+    })
+
+    const direccion = computed({
+      get: () => store.get('trabajador', 'direccion'),
+      set: (nuevaDir) => { trabajador.direccion = nuevaDir }
+    })
+
+    const fechaNac = computed({
+      get: () => store.get('trabajador', 'fechaNac'),
+      set: (nuevaFecha) => { trabajador.fechaNac = nuevaFecha }
+    })
+
+    const nacionalidad = computed({
+      get: () => store.get('trabajador', 'nacionalidad'),
+      set: (nuevaNacionalidad) => {store.set('trabajador', 'nacionalidad', nuevaNacionalidad)}
+    })
+
     return {
-      regionSeleccionada
+      regionSeleccionada,
+      comunaSeleccionada,
+      run,
+      nombres,
+      apellidoP,
+      apellidoM,
+      estadoCivil,
+      direccion,
+      fechaNac,
+      nacionalidad,
+      EstadoCivil
     }
   }
 })
