4efda4ba54181c89b490b3ae4f1cbfc7c80759f3
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-10 11:12:44 -0300

    declaracion de tipos y enums para el proyecto

---
+++ src/enums/index.ts
@@ -0,0 +1,22 @@
+export enum EstadoCivil {
+  Casado = 'casado/a',
+  Soltero = 'soltero/a',
+  Viudo = 'viudo/a',
+  Divorciado = 'divorciado/a'
+}
+
+export enum Turno {
+  SinTurno = 'sinTurno',
+  Manhana = 'manhana',
+  Tarde = 'tarde',
+  Noche = 'noche'
+}
+
+export enum RotacionTurnos {
+  Fijo = 'fijo',
+  Reglamento = 'regl',
+  ManTarde = 'mt',
+  ManNoche = 'mn',
+  TardeNoche = 'tn',
+  ManTardeNoche = 'mtn'
+}
---
+++ src/types/common.d.ts
@@ -0,0 +1,87 @@
+declare enum EstadoCivil {
+  Casado = 'casado/a',
+  Soltero = 'soltero/a',
+  Viudo = 'viudo/a',
+  Divorciado = 'divorciado/a'
+}
+
+declare enum Turno {
+  SinTurno = 'sinTurno',
+  Manhana = 'manhana',
+  Tarde = 'tarde',
+  Noche = 'noche'
+}
+
+declare enum RotacionTurnos {
+  Fijo = 'fijo',
+  Reglamento = 'regl',
+  ManTarde = 'mt',
+  ManNoche = 'mn',
+  TardeNoche = 'tn',
+  ManTardeNoche = 'mtn'
+}
+
+declare interface ContratoTrabajo {
+  empleador: Empleador,
+  trabajador: Trabajador,
+  condicionesContractuales: CondicionesContractuales
+}
+
+declare interface Comuna {
+  codigo: string,
+  nombre: string
+}
+
+declare interface Region {
+  codigo: string,
+  nombre: string
+}
+
+declare interface Empleador {
+  esPersonaJuridica: boolean,
+  razonSocial: string,
+  rut: string,
+  domicilio: string,
+  region: Region,
+  comuna: Comuna,
+  nombreRepLegal: string,
+  rutRepLegal: string,
+  cargoRepLegal: string
+}
+
+declare interface Trabajador {
+  run: string,
+  nombre: string,
+  apellidoPaterno: string,
+  apellidoMaterno: string,
+  nacionalidad: string,
+  estadoCivil: EstadoCivil,
+  sexo: string,
+  direccion: string,
+  fechaNac: string,
+  comuna: Comuna,
+  region: Region
+}
+
+declare interface CondicionesContractuales {
+  sueldoBase: number,
+  periodicidadSueldo: string,
+  cargo: string,
+  funciones: Array<string>,
+  direccionLabores: string,
+  regionLabores: Region,
+  comunaLabores: Comuna,
+  horasJornada: number,
+  jornada: RotacionTurnos | null,
+  horario: Horarios
+}
+
+declare type ArregloHorario = string[][]
+declare type Horarios = Record<Turno, ArregloHorario>
+
+declare type TurnoStrings = keyof typeof Turno
+declare type ContratoTrabajoKeys = keyof ContratoTrabajo
+declare type ContratoTrabajoSubKeys = keyof ContratoTrabajo[ContratoTrabajoKeys]
+declare type EmpleadorKeys = keyof Empleador
+declare type TrabajadorKeys = keyof Trabajador
+declare type CondificonesKeys = keyof CondicionesContractuales
