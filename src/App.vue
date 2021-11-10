9f57e4ba6e7b0f7d7e8ef1ce2fb0cf0996850fc7
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-10 11:16:50 -0300

    Agregado wrapper aplicacion

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="nav">
-    <h1>Contrato de Trabajo</h1>
-  </div>
-  <router-view />
+  <main
+    class="h-screen p-4 container mx-auto
+    flex flex-col items-center justify-center"
+  >
+    <router-view />
+  </main>
 </template>
