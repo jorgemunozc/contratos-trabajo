e42c8fc127ac0410e8f431a1feee367b8e4118f2
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-10 11:08:41 -0300

    Modificacion reglas eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,31 @@ module.exports = {
   env: {
     node: true
   },
+  parser: 'vue-eslint-parser',
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    extraFileExtensions: [
+      '.d.ts',
+      '.vue'
+    ]
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': ["error", {
+      "named": "never"
+    }],
+    'vue/html-self-closing': 'off',
+    // Enable vue/script-setup-uses-vars rule
+    'vue/script-setup-uses-vars': 'error',
+    'no-unused-vars': 'off'
   }
 }
