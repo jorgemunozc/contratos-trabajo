2b547e55a606523d94c3f140935c784f0f46e6bf
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-12-01 17:39:29 -0300

    Agregado estilos

--- src/views/GenerarContrato.vue
+++ src/views/GenerarContrato.vue
@@ -1,18 +1,40 @@
 <template>
-  <BarraProgreso />
-  <router-view />
-  <BotonesProgreso />
+  <div
+    class="
+    min-h-0 w-full max-w-3xl
+    flex-grow flex flex-col items-end gap-y-2
+    max-h-96
+  "
+  >
+    <BarraProgreso />
+    <router-view />
+    <BotonesProgreso />
+  </div>
+  <!-- <div class="w-full">
+    {{ test }}
+  </div> -->yay
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
 import BarraProgreso from '@/views/BarraProgreso.vue'
 import BotonesProgreso from '@/views/BotonesProgreso.vue'
+import store from '@/store/contrato'
+
 
 export default defineComponent({
   components: {
     BarraProgreso,
-    BotonesProgreso
+    BotonesProgreso,
+    // ContratoPreview
+  },
+  setup() {
+    // const horariosTurno = store.obtenerHorariosJornada(RotacionTurnos.ManTardeNoche)
+    // cargarFakeInfo()
+    const test = computed(() => store.state.condicionesContractuales)
+    return {
+      test
+    }
   }
 })
 </script>
