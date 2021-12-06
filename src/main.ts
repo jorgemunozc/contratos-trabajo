3475b10ea45df3c1afa2eb292e79e37bcbba6b86
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-12-06 10:04:24 -0300

    movido css de directorio

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './index.css'
+import './assets/index.css'
 createApp(App).use(router).mount('#app')
