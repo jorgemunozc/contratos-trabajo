86ed599d20ac1013a27b612d4c35def69682aa87
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-12-08 12:02:06 -0300

    Eliminado subtipo innecesario

--- src/services/ComunaService.ts
+++ src/services/ComunaService.ts
@@ -1,21 +1,15 @@
 import axios from 'axios'
-export interface ComunaInfo {
-  codigo: string,
-  nombre: string
-}
 
-export function cargarComunas(codRegion: string): Promise<ComunaInfo[] | []> {
+export function cargarComunas(codRegion: string): Promise<Comuna[]> {
   const url = `https://apis.digital.gob.cl/dpa/regiones/${codRegion}/comunas`
-  return axios.get<Record<string, string>[]>(url)
+  return axios.get<Comuna[]>(url)
     .then((response) => {
       const comunas = response.data
-      const comunasFiltradas: ComunaInfo[] = []
-      for (const comuna of comunas) {
-        comunasFiltradas.push({
-          codigo: comuna.codigo,
-          nombre: comuna.nombre
-        })
-      }
+      const comunasFiltradas = comunas.map((comuna) => ({
+        nombre: comuna.nombre,
+        codigo: comuna.codigo
+        }))
+
       return comunasFiltradas
     })
     .catch(() => [])
--- src/services/RegionService.ts
+++ src/services/RegionService.ts
@@ -1,24 +1,18 @@
 import axios from 'axios'
 
-interface RegionInfo {
-  nombre: string
-  codigo: string
-}
-
-function cargarRegiones(): Promise<RegionInfo[] | []> {
-  return axios.get<Record<string, string>[]>('https://apis.digital.gob.cl/dpa/regiones')
+function cargarRegiones(): Promise<Region[]> {
+  return axios.get<Region[]>('https://apis.digital.gob.cl/dpa/regiones')
     .then((response) => {
       const dataRegiones = response.data
-      const listaRegiones: RegionInfo[] = []
-      for (const region of dataRegiones) {
-        listaRegiones.push({
+      const listaRegiones: Region[] = dataRegiones.map((region) => {
+        return {
           nombre: region.nombre,
           codigo: region.codigo
-        })
-      }
+        }
+      })
       return listaRegiones
     })
     .catch(() => [])
 }
 
-export { cargarRegiones, RegionInfo }
+export { cargarRegiones }
