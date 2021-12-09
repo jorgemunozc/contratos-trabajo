30dbd28191b45f8323a59a634c02d485410cba6f
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-12-09 20:52:47 -0300

    Eliminado iframe de pdf anitugo

--- src/views/ContratoPreview.vue
+++ src/views/ContratoPreview.vue
@@ -1,40 +1,16 @@
 <template>
-  <section class="w-full flex-grow flex max-w-2xl justify-center">
-    <iframe
-      ref="iframePdf"
-      class="w-full"
-      type="application/pdf"
-    />
+  <section class="w-full flex-grow max-w-2xl overflow-auto mx-auto border">
+    <div class="transform scale-100">
+      <ContratoTrabajo />
+    </div>
   </section>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, watch } from 'vue'
-import ContratoPDF from '@/utils/pdf'
-import cargarFakeInfo from '@/utils/faker'
+import {defineComponent } from 'vue'
+import ContratoTrabajo from './ContratoTrabajo.vue'
 
 export default defineComponent({
-  setup() {
-    const iframePdf = ref<HTMLIFrameElement>()
-    // eslint-disable-next-line
-    const doc = new ContratoPDF()
-    function agregarHtml(): void {
-      if (iframePdf.value !== undefined) {
-        iframePdf.value.src = doc.output().toString()
-      }
-    }
-    watch(
-      () => iframePdf.value,
-      () => {
-        if (iframePdf.value){
-          iframePdf.value.src = doc.output().toString()
-        }
-      }
-    )
-    return {
-      iframePdf,
-      agregarHtml
-    }
-  }
+  components: { ContratoTrabajo },
 })
 </script>
