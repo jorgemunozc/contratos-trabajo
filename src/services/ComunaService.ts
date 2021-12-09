9a718d8b57a2ec0b9588057934ac9c613a45fe31
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-12-09 11:42:47 -0300

    Agregada opcion para obtener todas las comunas

--- src/services/ComunaService.ts
+++ src/services/ComunaService.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export function cargarComunas(codRegion: string): Promise<Comuna[]> {
-  const url = `https://apis.digital.gob.cl/dpa/regiones/${codRegion}/comunas`
+  let url = `https://apis.digital.gob.cl/dpa/regiones/${codRegion}/comunas`
+  if (codRegion == '-1') {
+    url = 'https://apis.digital.gob.cl/dpa/comunas'
+  }
   return axios.get<Comuna[]>(url)
     .then((response) => {
       const comunas = response.data
